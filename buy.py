664d70121a40f5c901764cb69d0b7ca8ee174677
Vincent <vincentabet@gmail.com>
2021-04-29 17:42:21 +0200

    update buy.py

--- buy.py
+++ buy.py
@@ -1,4 +1,5 @@
 from binance.client import Client
+import binance.exceptions
 import config
 import calculator
 import json
@@ -9,15 +10,22 @@ client = Client(config.apiKey, config.apiSecurity)
 
 
 balance = client.get_asset_balance(asset='USDT')
-print("Balance dispinible : " + str(balance['free']))
+print("Balance dispinible : " + str(balance['free']) + "USDT")
 moneyToBuyInUSDT = calculator.get_top_moneyToBuyInUSDT(
     float(balance['free']), config.strategyTop)
 moneyBuyInCoin = calculator.get_empty_moneydict()
+conti = False
 for coin, value in moneyToBuyInUSDT.items():
-    print("Achat de " + coin + " : " + str(value))
-    order = client.order_market_buy(
-        symbol=coin+'USDT', quoteOrderQty=value)
-    moneyBuyInCoin[coin] = order['origQty']
+    try:
+        info = client.get_symbol_info(coin+'USDT')
+        valueWithPrecision = round(value, info['quoteAssetPrecision'])
+        print("Achat de " + coin + " : " +
+              str(valueWithPrecision) + " USDT")
+        order = client.order_market_buy(
+            symbol=coin+'USDT', quoteOrderQty=valueWithPrecision)
+        moneyBuyInCoin[coin] = order['origQty']
+    except binance.exceptions.BinanceAPIException as error:
+        print(error.message)
 
 logs = [moneyBuyInCoin, moneyToBuyInUSDT]
 with open('./logs/'+today.strftime("%Y-%m-%d")+'_data.json', 'w') as outfile:
